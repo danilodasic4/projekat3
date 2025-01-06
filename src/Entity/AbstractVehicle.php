167d3ddf53f3cc3d773f89cd58a36f2357ed7ce1
Danilo Dasic <danilodasic8@gmail.com>
2025-01-06 16:56:49 +0100

    Finished second version

--- src/Entity/AbstractVehicle.php
+++ src/Entity/AbstractVehicle.php
@@ -4,40 +4,5 @@ namespace App\Entity;
 
 abstract class AbstractVehicle
 {
-    private ?string $brand = null;
-    private ?string $model = null;
-    private ?int $year = null;
-
-    public function getBrand(): ?string
-    {
-        return $this->brand;
-    }
-
-    public function setBrand(string $brand): static
-    {
-        $this->brand = $brand;
-        return $this;
-    }
-
-    public function getModel(): ?string
-    {
-        return $this->model;
-    }
-
-    public function setModel(string $model): static
-    {
-        $this->model = $model;
-        return $this;
-    }
-
-    public function getYear(): ?int
-    {
-        return $this->year;
-    }
-
-    public function setYear(int $year): static
-    {
-        $this->year = $year;
-        return $this;
-    }
+    //For now i will let it be empty, why changing destination of getters and setters, when i have all that in Car Entity
 }
