d73e98542ccb480e6506fd6e6796a7a597b73524
Danilo Dasic <danilodasic8@gmail.com>
2025-01-08 10:58:06 +0100

    All atributes removed from car

--- src/Entity/AbstractVehicle.php
+++ src/Entity/AbstractVehicle.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use OpenApi\Attributes as OA;
 
 #[ORM\MappedSuperclass]
 abstract class AbstractVehicle
@@ -10,29 +11,54 @@ abstract class AbstractVehicle
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[OA\Property(description: "The unique identifier of the vehicle")]
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[OA\Property(description: "Brand of the vehicle")]
     protected ?string $brand = null;
 
     #[ORM\Column(length: 255)]
+    #[OA\Property(description: "Model of the vehicle")]
     protected ?string $model = null;
 
     #[ORM\Column]
+    #[OA\Property(description: "Year of manufacture")]
     protected ?int $year = null;
 
     #[ORM\Column]
+    #[OA\Property(description: "Engine capacity in cubic centimeters")]
     protected ?int $engineCapacity = null;
 
     #[ORM\Column]
+    #[OA\Property(description: "Horse power of the vehicle")]
     protected ?int $horsePower = null;
 
     #[ORM\Column(length: 50, nullable: true)]
+    #[OA\Property(description: "Color of the vehicle", nullable: true)]
     protected ?string $color = null;
 
     #[ORM\Column(type: "date")]
+    #[OA\Property(type: "string", format: "date", description: "Registration date of the vehicle")]
     protected ?\DateTimeInterface $registrationDate = null;
-    
+
+    #[ORM\Column(type: "datetime")]
+    #[OA\Property(type: "string", format: "date-time", description: "Creation timestamp")]
+    protected ?\DateTimeInterface $created_at = null;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    #[OA\Property(type: "string", format: "date-time", description: "Last updated timestamp", nullable: true)]
+    protected ?\DateTimeInterface $updated_at = null;
+
+    #[ORM\Column(type: "datetime", nullable: true)]
+    #[OA\Property(type: "string", format: "date-time", description: "Timestamp when the vehicle was deleted, if applicable", nullable: true)]
+    protected ?\DateTimeInterface $deleted_at = null;
+
+    #[ORM\ManyToOne(targetEntity: User::class, inversedBy: 'cars')]
+    #[ORM\JoinColumn(nullable: false)] 
+    #[OA\Property(ref: "#/components/schemas/User", description: "User who owns the car")]
+    private ?User $user = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -114,4 +140,50 @@ abstract class AbstractVehicle
         $this->registrationDate = $registrationDate;
         return $this;
     }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updated_at;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updated_at): self
+    {
+        $this->updated_at = $updated_at;
+        return $this;
+    }
+
+    public function getDeletedAt(): ?\DateTimeInterface
+    {
+        return $this->deleted_at;
+    }
+
+    public function setDeletedAt(?\DateTimeInterface $deleted_at): self
+    {
+        $this->deleted_at = $deleted_at;
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+        return $this;
+    }
+
 }
+
