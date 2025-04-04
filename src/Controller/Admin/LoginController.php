c1fbcf2e87dedba7a5f41f093cac9af9df9146d7
Danilo Dasic <danilodasic8@gmail.com>
2025-04-04 19:59:50 +0200

    Less seconds for redis lock of admins logging

--- src/Controller/Admin/LoginController.php
+++ src/Controller/Admin/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends AbstractController
             return $this->redirectToRoute('admin_index');
         }
 
-        $lock = $this->lockFactory->createLock(self::ADMIN_LOCK_KEY, 300, false);
+        $lock = $this->lockFactory->createLock(self::ADMIN_LOCK_KEY, 180, false);
 
         if (!$lock->acquire(false)) {
             // If the lock is already acquired, show the error template
